51cd60dbe60f89d81d03e700efaaf580961642b7
iopeti2u <iopeti2u@etu.univ-lorraine.fr>
2022-03-03 09:14:14 +0100

    Création de la VueEvenement.php

---
+++ src/views/VueEvenement.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace goldenppit\views;
+
+class VueEvenement
+{
+    private $tab;
+    private $container;
+
+    /**
+     * VueEvenement constructor.
+     * @param $tab
+     * @param $container
+     */
+    public function __construct($tab, $container)
+    {
+        $this->tab = $tab;
+        $this->container = $container;
+    }
+
+    /**
+     * RENDER
+     * @param int $select
+     * @return string
+     */
+    public function render(int $select): string
+    {
+        $url_accueil = $this->container->router->pathFor('racine');
+        $url_formInsription = $this->container->router->pathFor('formInscription');
+        $url_formConnexion = $this->container->router->pathFor('formConnexion');
+        $content = "";
+        switch ($select) {
+            case 0: //
+            {
+                $content .= $this->formulaireEvenement();
+                break;
+            }
+        }
+
+        $html = <<<FIN
+<html>
+
+<head>
+    <title>GoldenPPIT</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <nav>
+        <div class ="container">
+            <div class="logo">
+                <a href="$url_accueil" title="logo">
+                    <img src="images/logo-white.png" >
+                </a>
+            </div>
+
+            <div class="menu text-right">
+                <ul>
+                    <li> <a href="$url_formConnexion"> Se connecter </a></li>
+                    <li> <a href="$url_formInsription"> S'inscrire </a> </li>      
+                </ul>
+            </div>
+
+            <div class="clearfix"></div>
+        </div>
+    </nav>
+    <div class = "content-wrap">
+        $content
+    </div>
+    <footer>
+    <div class="clearfix"></div>
+        <div class="container text-center">
+                <a href="#"> Nous contacter </a>
+                <a href="#"> A propos de nous </a>
+                <p> © 2022 GoldenPPIT. Tous droits réservés </p>
+        </div>    
+    
+    </footer>
+</body>
+</html>
+FIN;
+
+        return $html;
+    }
+
+    public function formulaireEvenement(): string
+    {
+        $url_enregistrerEvenement = $this->container->router->pathFor('enregistrerEvenement');
+
+        $html = <<<FIN
+<h1 class="text-center">Créer un événement</h1>
+		<div class = "container ">
+		
+		<form method="post" action="$url_enregistrerEvenement">
+			<fieldset class="fieldset">
+				<div class="field"> 
+				    <label> Nom * :  </label>
+				    <input type="text" name="nom" placeholder="Nom de l'événement" pattern="[a-ZA-Z]+" required="required"/>
+                </div>
+				
+				<div class="field"> 
+				    <label> Date de début * : </label>
+				    <input type="date" name="deb" placeholder="03-03-2022" required="required"/>
+				</div>
+				
+				<div class="field"> 
+				    <label> Date de fin * : </label>
+				    <input type="date" name="fin" placeholder="08-03-2022" required="required"/>
+				</div>
+				
+				<div class="field"> 
+				    <label> Date de suppression automatique : </label>
+				    <input type="date" name="supprauto" placeholder="24-04-2022" />
+				</div>
+				
+				<div class="field"> 
+				    <label> Date d'archivage: </label>
+				    <input type="date" name="achiv" placeholder="24-04-2022" />
+				</div>
+				
+				<div class="field"> 
+				    <label> Lieu * : </label>
+				    <input type="text" name="lieu" placeholder="Lieu de l'évenement" required="required"/>
+				</div>
+				
+				<div class="field"> 
+				    <label> Description : </label>
+				    <input type="textfield" name="lieu" placeholder="Décrivez votre événement en quelques mots !"/>
+				</div>
+				
+				<span class="span text-right"> *  : Champ obligatoire !</span>
+			</fieldset>
+			
+            <div class="clearfix"/>
+            
+			<input type="submit" value="Créer" name="submit" class="bouton-bleu" />
+		</form>
+
+    </div>
+    
+</body>
+FIN;
+        return $html;
+    }
+}
