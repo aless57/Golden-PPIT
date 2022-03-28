58231c5fd4177e0f3a711b87e72e3b5cd2817bf4
Didier Manzi <didier.manzi2@etu.univ-lorraine.fr>
2022-03-28 17:58:28 +0200

    Ajout des fonctions enregistrerModifEvenement() et modifEvenement() dans la classe ControlleurEvenement.php

--- src/controllers/ControlleurEvenement.php
+++ src/controllers/ControlleurEvenement.php
@@ -174,6 +174,60 @@ class ControlleurEvenement
         return true;
     }
 
+    /**
+     * POST
+     * Modification de l'evenement dans la BDD
+     * @param Request $rq
+     * @param Response $rs
+     * @param $args
+     * @return Response
+     */
+    public function enregistrerModifEvenement(Request $rq, Response $rs, $args): Response
+    {
+        $id_ev = $args['id_ev'];
+
+        $post = $rq->getParsedBody();
+
+        $nom = filter_var($post['nom'], FILTER_SANITIZE_STRING);
+        $debut = filter_var($post['deb'], FILTER_SANITIZE_STRING);
+        $archiv = filter_var($post['archiv'], FILTER_SANITIZE_STRING);
+        $supprAuto = filter_var($post['supprauto'], FILTER_SANITIZE_STRING);
+        $lieu = filter_var($post['lieu'], FILTER_SANITIZE_STRING);
+        $desc = filter_var($post['desc'], FILTER_SANITIZE_STRING);
+
+        $vue = new VueAccueil([], $this->container);
+
+        if ($this->modifEvent($id_ev, $nom, $debut, $archiv, $supprAuto, $lieu, $desc)) { //si tout est bon, on affiche la page de l'évenement
+            $url_accueil = $this->container->router->pathFor("evenement", ['id_ev' => $id_ev]);
+            return $rs->withRedirect($url_accueil);
+        } else {
+            $rs->getBody()->write($vue->render(2));
+        }
+        return $rs;
+    }
+
+    public function modifEvent($id_ev, $nom, $debut, $archiv, $supprAuto, $lieu, $desc){
+        $e = Evenement::where('e_id', '=', $id_ev)->first();
+
+        if ($supprAuto != null) {
+            $e->e_supp_date = $supprAuto;
+        }
+
+        if ($desc != null) {
+            $e->e_desc = $desc;
+        }
+
+        $e->e_titre = $nom;
+        $e->e_date = $debut;
+        $e->e_archive = $archiv;
+        $e->e_statut = ENCOURS;
+
+        $e->e_ville = Ville::where('v_nom', '=', $lieu)->first()->v_id;
+
+        $e->save();
+
+        return true;
+    }
 
 
     /**
